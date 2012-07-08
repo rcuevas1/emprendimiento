efa2791a0c07afe96d66c4d493776aa0be785be2
RodrigoCuevas <rcuevas1@uc.cl>
2012-07-08 18:26:14 -0400

    terminado layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Test1::Application.routes.draw do
   resources :ideas
-
   resources :users
+
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
 
   get "static_pages/home"
   get "static_pages/help"
