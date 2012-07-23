a334ee6c9715f5e697a04cc61108f420ec54df12
RodrigoCuevas <rcuevas1@uc.cl>
2012-07-23 13:49:53 -0400

    ideas visibles por todo y arreglado el homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
