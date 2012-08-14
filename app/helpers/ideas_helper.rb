43bb48c7d2f7fd020c8a3ab137271ab3cc1d72a5
Rodrigo <rcuevas1@uc.cl>
2012-08-14 00:40:08 -0400

    m3

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,6 +1,6 @@
 module IdeasHelper
 
-def facebook_like
-            content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+def facebook_like(idea_url)
+            content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{idea_url}&layout=standard&show_faces=false&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
           end
 end
