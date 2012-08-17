48081744d467b3222a9a98e22c5481230db85fd6
Rodrigo <rcuevas1@uc.cl>
2012-08-16 22:00:43 -0400

    terminado slides

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,12 +1,8 @@
 class Brand < ActiveRecord::Base 
-
   attr_accessible :description, :name, :image
   validates :name, presence: true
   validates :description, presence: true  
   has_many :ideas
   has_attached_file :image,
-	:styles => {
-      :mini=> "50x50#",
-      :thumb=> "100x100#",
-      :small  => "150x150>" }
+	:styles => {:mini=> "50x50#",:thumb=> "100x100#",:small  => "150x150>" }
 end
