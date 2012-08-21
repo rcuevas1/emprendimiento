85d72234a3665d94d15df72af5366dd4cd51b5af
Rodrigo <rcuevas1@uc.cl>
2012-08-20 22:26:20 -0400

    error home sin login2

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,7 +2,7 @@ class Brand < ActiveRecord::Base
   attr_accessible :description, :name, :image
   validates :name, presence: true
   validates :description, presence: true  
-  has_many :ideas
+  has_many :ideas, dependent: :destroy
   has_attached_file :image,
 	:styles => {:mini=> "50x50#",:thumb=> "100x100#",:small  => "150x150>" }
 	
